260873130fddb03bad6e3adc12301137f126ae0f
Pilar Rangga Saputra <pilar@student.uns.ac.id>
2022-03-09 08:58:36 +0700

    code

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,9 @@ void main() {
   runApp(const MyApp());
 }
 
-// #docregion MyApp
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // #docregion build
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
@@ -21,50 +19,39 @@ class MyApp extends StatelessWidget {
       home: RandomWords(),
     );
   }
-  // #enddocregion build
 }
-// #enddocregion MyApp
 
-// #docregion RWS-var
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
-  // #enddocregion RWS-var
 
-  // #docregion RWS-build
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Startup Name Generator'),
       ),
-      // #docregion itemBuilder
+
       body: ListView.builder(
         padding: const EdgeInsets.all(16.0),
         itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return const Divider(); /*2*/
+          if (i.isOdd) return const Divider(); 
 
-          final index = i ~/ 2; /*3*/
+          final index = i ~/ 2; 
           if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+            _suggestions.addAll(generateWordPairs().take(10)); 
           }
-          // #docregion listTile
           return ListTile(
             title: Text(
               _suggestions[index].asPascalCase,
               style: _biggerFont,
             ),
           );
-          // #enddocregion listTile
         },
       ),
-      // #enddocregion itemBuilder
     );
   }
-  // #enddocregion RWS-build
-  // #docregion RWS-var
+
 }
-// #enddocregion RWS-var
 
 class RandomWords extends StatefulWidget {
   const RandomWords({Key? key}) : super(key: key);
@@ -72,5 +59,3 @@ class RandomWords extends StatefulWidget {
   @override
   _RandomWordsState createState() => _RandomWordsState();
 }
-
-//pilar
